6da9fb9b524246d03dd00e5dbc0708adcd5134d5
Mohammad Hamza Tariq <130709648+hamza-092@users.noreply.github.com>
2023-04-14 00:43:43 +0200

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,3 @@
+city = input('what is your city name that you grew up in ? \n')
+pet_name = input('what is the name of your pet or the name you will keep if yoy want one ? \n')
+print('Your band name could be ' + city + ' ' + pet_name)
